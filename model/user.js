e39d05eef45e9e648e2f3c7b3083935b687d4c3d
srushti-dhakate-2210 <dhakatesrushti22@gmail.com>
2023-07-17 14:52:04 +0530

    db issue resolved

--- model/user.js
+++ model/user.js
@@ -1,17 +1,10 @@
-import mongoose from 'mongoose';
-import autoIncrement from 'mongoose-auto-increment';
+import mongoose from "mongoose";
 
-// how our document look like
-const userSchema = mongoose.Schema({
-    name: String,
-    username: String,
-    email: String,
-    phone: Number
+const userSchema = new mongoose.Schema({
+  name: String,
+  username: String,
+  email: String,
+  phone: Number,
 });
 
-autoIncrement.initialize(mongoose.connection);
-userSchema.plugin(autoIncrement.plugin, 'user');
-// we need to turn it into a model
-const postUser = mongoose.model('user', userSchema);
-
-export default postUser;
+export default mongoose.model("user", userSchema);
